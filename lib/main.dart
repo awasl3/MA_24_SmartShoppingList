753245a1d43a6e7f14a4cd9a6098bae62881a805
Anton Wasl <awasl@hm.edu>
2024-06-25 21:13:21 +0200

    added simple inventory

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:path/path.dart';
 import 'package:smart_shopping_list/util/routing/router.dart';
+import 'package:sqflite/sqflite.dart';
 
-void main() {
+late final Future<Database> database;
+
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+   database = openDatabase(
+   join(await getDatabasesPath(), 'article_database.db'),
+   onCreate: (db, version) {
+    // Run the CREATE TABLE statement on the database.
+    return db.execute(
+      'CREATE TABLE articles(name TEXT PRIMARY KEY, currentAmount REAL, dailyUsage REAL, unit TEXT, rebuyAmount REAL)',
+    );
+  },
+   version: 1,
+  );
   runApp(const MyApp());
 }
 
--- lib/pages/inventory/inventory.dart
+++ lib/pages/inventory/inventory.dart
@@ -1,11 +1,42 @@
-import 'package:flutter/material.dart';
+import 'dart:math';
 
-class Inventory extends StatelessWidget {
-  const Inventory({super.key});
+import 'package:flutter/material.dart';
+import 'package:smart_shopping_list/pages/inventory/stock/article.dart';
+import 'package:smart_shopping_list/pages/inventory/stock/article_database.dart';
+
+class InventoryPage extends StatelessWidget {
+  const InventoryPage({super.key});
   
   @override
   Widget build(BuildContext context) {
-    return const Text("Inventory");
+   return FutureBuilder<List<Article>>(
+      future: getAllArticles(),
+      builder: (ctx, snapshot) {
+
+        if(snapshot.connectionState == ConnectionState.done) {
+          String text = "";
+          for (var element in snapshot.data!) {
+            text += "$element\n"; 
+          }
+          return  Text(text);       
+        }
+           
+        return const Text('No data available yet...');
+      }
+    );
+  }
+
+
+
+  Future<List<Article>> getAllArticles() async {
+   
+    List<Article> articles = await ArticleDatabase.getAllArticles();
+    if(articles.length < 20) {
+       Article a = Article(name: "Milk random_${Random().nextInt(100)}", currentAmount: 3.5, dailyUsage: 0.3, unit: "Liter", rebuyAmount: 1.0);
+      await ArticleDatabase.insertArticle(a);
+    }
+    return await ArticleDatabase.getAllArticles();
+    
   }
   
 }
