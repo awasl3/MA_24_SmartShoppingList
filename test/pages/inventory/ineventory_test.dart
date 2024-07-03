375bf8695d84f76a3760e11d6329225bb0be61a9
Anton Wasl <awasl@hm.edu>
2024-07-03 16:52:08 +0200

    fixed inventory page tests

--- test/pages/inventory/ineventory_test.dart
+++ test/pages/inventory/ineventory_test.dart
@@ -68,6 +68,115 @@ void main() {
         [article]);
     verify(mock.getAllArticles()).called(1);
   });
+
+  testWidgets('Inventory page has Add Button', (tester) async {
+     await GetIt.I.reset();
+    MockArticleDatabse mock = MockArticleDatabse();
+    when(mock.getAllArticles()).thenAnswer((_) => Future(() => [article]));
+
+    GetIt.I.registerSingleton<ArticleDatabse>(mock);
+
+    Widget testWidget = getTestWidget([
+      articlesChanged.overrideWith((ref) {
+        return false;
+      })
+    ]);
+    await tester.pumpWidget(testWidget);
+    final button = find.byType(FloatingActionButton).evaluate().first.widget
+        as FloatingActionButton;
+    expect(find.byType(FloatingActionButton), findsOneWidget);
+    expect((button.child as Icon).icon, Icons.add);
+
+    await tester.tap(find.byType(FloatingActionButton));
+    await tester.pumpAndSettle();
+
+    expect(find.byType(AlertDialog), findsOneWidget);
+
+    expect(find.text('Name must be provided'), findsOneWidget);
+    expect(find.text('Current amount must be provided'), findsOneWidget);
+    expect(find.text('Create Article'), findsOneWidget);
+  });
+
+  testWidgets('Inventory page has Delete Button', (tester) async {
+    await GetIt.I.reset();
+    MockArticleDatabse mock = MockArticleDatabse();
+    when(mock.getAllArticles()).thenAnswer((_) => Future(() => [article]));
+
+    GetIt.I.registerSingleton<ArticleDatabse>(mock);
+
+    Widget testWidget = getTestWidget([
+      articlesChanged.overrideWith((ref) {
+        return false;
+      }),
+       articleDeletionMode.overrideWith((ref) {
+        return true;
+      })
+    ]);
+
+    await tester.pumpWidget(testWidget);
+    final button = find.byType(FloatingActionButton).evaluate().first.widget
+        as FloatingActionButton;
+    expect(find.byType(FloatingActionButton), findsExactly(2));
+    expect(button.backgroundColor, Colors.redAccent);
+    expect(button.heroTag, "deletionButton");
+    expect((button.child as Icon).icon, Icons.delete_forever);
+    await tester.tap(find.byType(FloatingActionButton).first);
+    await tester.pumpAndSettle();
+
+    expect(find.byType(AlertDialog), findsOneWidget);
+    expect(
+        find.text(
+            'Are you sure, you want to deleted the selected articles from the stock.\n! This action can not be undone !'),
+        findsOneWidget);
+    expect(find.text('Delete Articles'), findsOneWidget);
+  });
+
+  testWidgets('Deletion Dialog can be canceled', (tester) async {
+    await GetIt.I.reset();
+    MockArticleDatabse mock = MockArticleDatabse();
+    when(mock.getAllArticles()).thenAnswer((_) => Future(() => [article]));
+
+    GetIt.I.registerSingleton<ArticleDatabse>(mock);
+
+    Widget testWidget = getTestWidget([
+      articlesChanged.overrideWith((ref) {
+        return false;
+      }),
+      articleDeletionSelection.overrideWith((ref) {
+        return [article];
+      }),
+       articleDeletionMode.overrideWith((ref) {
+        return true;
+      })
+    ]);
+
+    await tester.pumpWidget(testWidget);
+    final button = find.byType(FloatingActionButton).evaluate().first.widget
+        as FloatingActionButton;
+    expect(find.byType(FloatingActionButton), findsExactly(2));
+    expect(button.backgroundColor, Colors.redAccent);
+    expect((button.child as Icon).icon, Icons.delete_forever);
+    await tester.tap(find.byType(FloatingActionButton).first);
+    await tester.pumpAndSettle();
+
+    expect(find.byType(AlertDialog), findsOneWidget);
+    expect(
+        find.text(
+            'Are you sure, you want to deleted the selected articles from the stock.\n! This action can not be undone !'),
+        findsOneWidget);
+    expect(find.text('Delete Articles'), findsOneWidget);
+
+    await tester.tap(find.text("Cancel"));
+    await tester.pumpAndSettle();
+    expect(find.byType(AlertDialog), findsNothing);
+
+    final element = tester.element(find.byType(InventoryPage));
+    final container = ProviderScope.containerOf(element);
+
+    expect(container.read(articleDeletionSelection).length, 1);
+    expect(container.read(articleDeletionMode), true);
+    expect(container.read(articlesChanged), false);
+  });
 }
 
 Widget getTestWidget(List<Override> overrides) {
